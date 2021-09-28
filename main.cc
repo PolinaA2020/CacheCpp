ef6edbfd22885aa628c009f6852fca883d7eacaa
PolinaA2020 <alpatova.pg@phystech.edu>
2021-09-28 13:36:47 +0300

    simple tests added, alg not correct

--- main.cc
+++ main.cc
@@ -1,7 +1,26 @@
 // main for LIRSCache
-#include "LIRSCache.cc"
 #include "LIRSCache.hpp"
+#include <cstring>
+#include <cassert>
+#include <fstream>
 
+enum TypeOfValue {
+	TYPE_OF_VALUE_INT,
+	TYPE_OF_VALUE_FLOAT,
+	TYPE_OF_VALUE_STRING,
+	TYPE_OF_VALUE_ERORR,
+};
+
+const std::string typeOfValueArr[] = {"int", "float", "string"};
+/*============================================================*/
+int getTypeOfValue(const std::string type_str)
+{
+	for(int i = 0; i < TYPE_OF_VALUE_ERORR; i++) {
+		if(type_str == typeOfValueArr[i])
+			return i;
+	}
+	return TYPE_OF_VALUE_ERORR;
+}
 /*============================================================*/
 template <typename Tt>
 Tt slow_get_page(Tt key)
@@ -10,46 +29,49 @@ Tt slow_get_page(Tt key)
 }
 /*============================================================*/
 template <typename T>
-	int countCacheHits(size_t size)
+	int countCacheHits(size_t size, std::ifstream &f)
 	{
 		caches::cache_<T, T> c{size};
 		T val;
-		std::cout << "Enter number of elements\n";
-		int N;
-		std::cin >> N;
-		std::cout << "Enter value\n";
-		
-		for(int i = 0; i < N; i++) {
-			std::cin >> val;
-			c.lookUpAndUpdate(val, slow_get_page<T>);
+		//std::cout << "Enter number of elements\n";
+		//int N;
+		//std::cin >> N;
+		//std::cout << "Enter value\n";
+		unsigned int hit = 0;
+		for( ; ; ){
+			f >> val;
+			if (f.eof())
+				break;
+			hit += c.lookUpAndUpdate(val, slow_get_page<T>);
 			//c.template lookUpAndUpdate<F>(val, type_get_func); 
 		}
 		c.printCache();
-		return 0;
+		return hit;
 	}
 /*============================================================*/
 int main(int argc, char** argv) {
 	std::string type_of_value_str = argv[1];
 	size_t size = atoi(argv[2]);
-	
+	std::ifstream source(argv[3]); 
 	int type_of_value = getTypeOfValue(type_of_value_str);
 	assert(type_of_value != TYPE_OF_VALUE_ERORR);
 
 	unsigned int hit = 0;
 	switch(type_of_value) {
 	case TYPE_OF_VALUE_INT: {
-		hit = countCacheHits<int>(size);
+		hit = countCacheHits<int>(size, source);
 		break;
 	}
 	case TYPE_OF_VALUE_FLOAT: {
-		hit = countCacheHits<float>(size);
+		hit = countCacheHits<float>(size, source);
 		break;
 	}
 	case TYPE_OF_VALUE_STRING: {
-		hit = countCacheHits<std::string>(size);
+		hit = countCacheHits<std::string>(size, source);
 		break;
 	}
 	}
-	cout << "Hits in cache: " << hit << std::endl;
+	std::cout << "Hits in cache: " << hit << std::endl;
+	source.close();
 	return 0;
 }
